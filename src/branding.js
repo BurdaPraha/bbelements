d5b886177d9f009e9a68317205755c9f22814c6b
Michal Landsman <landsman@insuit.cz>
2017-03-31 13:22:40 +0200

    added bbelements variables, prepared for store with site or external loading

--- src/branding.js
+++ src/branding.js
@@ -1,19 +1,29 @@
 /**
  * Example use !!!
  */
-createBranding({
+BD_CreateBranding({
     type: 'maruska',
     creative: {
         first: {
-            file: 'Pandora_Spring-Marianne-Branding_.jpg'
+            file: 'Marianne_Branding_1700x1200.jpg'
         },
         megaboard: {
             file: '',
-            height: 165
+            height: 150
         },
         background_color: '#fff'
     },
-    pixel: 'http://gdecz.hit.gemius.pl/_[TIMESTAMP]/redot.gif?id=1vObGes8yDiRLD8fOaf3ltTa3wi1KaMZ9MUD9r328Gv.q7/fastid=hhyvsrheeqohtafvqysmwumulfnn/stparam=ulfoelptoo'
+    tools: {
+        pr_height: 0
+    },
+    pixel: 'https://track.adform.net/adfserve/?bn=16817086;1x1inv=1;srctype=3;ord=[timestamp]',
+    ad_server: {
+        url: '%%__URL%%',
+        click_url: '%%__REDIRECT%%',
+        click_url_encoded: '%%__REDIRECT_ENCODED%%',
+        cd_ad_domain: '%%__CDN_AD_DOMAIN%%'
+    },
+    dev: true
 });
 
 
@@ -23,14 +33,14 @@ createBranding({
  * @param target
  * @returns {*}
  */
-function extend (target) {
+function BD_Extend(target) {
     for(var i=1; i<arguments.length; ++i) {
         var from = arguments[i];
         if(typeof from !== 'object') continue;
         for(var j in from) {
             if(from.hasOwnProperty(j)) {
                 target[j] = typeof from[j]==='object'
-                    ? extend({}, target[j], from[j])
+                    ? BD_Extend({}, target[j], from[j])
                     : from[j];
             }
         }
@@ -39,11 +49,11 @@ function extend (target) {
 }
 
 /**
- * Create css factory, todo: Object.keys(cssObject).reduce(fn, default)
+ * Create css factory, todo: Object.keys(cssObject).reduce(fn, default), todo: media query
  * @param cssObject
  * @returns {string}
  */
-function generateCSS(cssObject)
+function BD_GenerateCSS(cssObject)
 {
     var css = '';
     for (cName in cssObject)
@@ -71,180 +81,214 @@ function generateCSS(cssObject)
  * Create branding factory
  * @param myOptions object
  */
-function createBranding(myOptions)
+function BD_CreateBranding(myOptions)
 {
+    // default states
+    var is_front_page = false;
+    var is_gallery    = false;
+
     const defaultOptions = {
-        type: '',
+        type:                       '',
         creative: {
             first: {
-                file: '',
-                width: 1700,
-                height: 1200                
+                file:               '',
+                width:              1700,
+                height:             1200
             },
             second: {
-                file: '',
-                width: 1700,
-                height: 1200
+                file:               '',
+                width:              1700,
+                height:             1200
             },
             megaboard: {
-                file: '',
-                width: 980,
-                height: 120
+                file:               '',
+                width:              1000,
+                height:             120
             },
-            background_color: '#fff',
-            background_options: ''
+            background_color:       '#fff',
+            background_options:     ''
         },
         tools: {
-            height: 1200,
-            pr_word: 'Promotion',
-            pr_height: 15,
-            drupal_toolbar_height: 80
+            height:                 1200,
+            pr_word:                'Promotion',
+            pr_height:              15,
+            drupal_toolbar_height:  80,
+            element_id:             'page',
+            class_front_page:       'front-page',
+            class_gallery:          'gallery',
+            class_active:           'branding-active',
+            class_toolbar:          'toolbar-horizontal'
         },
-        pixel: '',
-        page_width: 980, // @todo: think about dynamic looking
-        element_class: 'branding_wrapper',
-        tpl_shared: true,
-        css: '',
-        html: ''
+        pixel:                      '',
+        ad_server: {
+            url:                    '',
+            click_url:              '',
+            click_url_encoded:      '',
+            cd_ad_domain:           ''
+        },
+        page_width:                 980, // @todo: think about dynamic looking
+        element_class:              'branding_wrapper',
+        tpl_shared:                 true,
+        dev:                        false,
+        css:                        '',
+        html:                       ''
     };
 
     // merge objects
-    var args = extend({}, defaultOptions, myOptions);
-    
+    var args = BD_Extend({}, defaultOptions, myOptions);
+
     // vyska odsazeni webu
     //args.creative.megaboard.height += args.tools.pr_height;
-    
+
+    // is it gallery page?
+    // get main wrapper
+    var body = document.body;
+    var page = document.getElementById(args.tools.element_id);
+
+    if(!page)
+    {
+        if(args.dev){
+            console.warn("element #" + args.tools.element_id + " not exists!")
+        }
+    }
+    else
+    {
+        is_front_page = page.classList.contains(args.tools.class_front_page);
+        is_gallery    = page.classList.contains(args.tools.class_gallery);
+    }
+
+    // zero padding
+    if(is_gallery)
+    {
+        args.tools.pr_height = 0;
+    }
+
     // allowed with superpowers
     switch(args.type)
     {
         // without action
         case 'anicka':
-            args.creative.background_options = 'no-repeat center ' + args.tools.pr_height + 'px'; 
-        break;
-        
+            args.creative.background_options = 'no-repeat center ' + args.tools.pr_height + 'px';
+            break;
+
         // fixed bg
         case 'maruska':
             args.creative.background_options = 'no-repeat center ' + args.tools.pr_height + 'px'; //fixed
-        break;
-            
+            break;
+
         // auto replicated
         case 'zuzanka':
-            args.tools.pr_height = 10;
             args.creative.background_options = 'repeat-y center ' + args.tools.pr_height + 'px';
-        break;
-            
+            break;
+
         // with HTML banner to header
         case 'amalka':
-        
+
             // check if valid
             if('' == args.creative.megaboard.file || args.creative.megaboard.file.indexOf('.html') !== -1){return alert('Please fill in creative.megaboard item file')}
-            
+
             args.css+= '' +
                 '.branding__megaboard iframe {margin:0px; border:0px}';
-            args.html+='' +
-                '<div class="branding__megaboard">' +
-                '<iframe ' +
-                        'src="%%URL%%' + args.creative.megaboard.file + '?redir=%%__REDIRECT_ENCODED%%&bbtarget=_blank" ' +
-                        'width="' + args.creative.megaboard.width + '" ' +
-                        'height="' + args.creative.megaboard.height + '" ' +
-                        'scrolling="no" ' +
-                        'seamless>' +
-                '</iframe>' +
-                '</div>';
             
-        break;
-            
+            args.html+="" +
+                + "<div class='branding__megaboard'>"
+                + "<iframe"
+                + " src='" + args.creative.megaboard.file + "?redir=" + args.ad_server.click_url_encoded + "&bbtarget=_blank'"
+                + " width='" + args.creative.megaboard.width + "'"
+                + " height='" + args.creative.megaboard.height + "'"
+                + " scrolling='no'"
+                + " seamless>"
+                + "</iframe>"
+                + "</div>";
+
+            break;
+
         // strip - not working right now, @todo!
         case 'rozarka':
-            
+
             var backgroundDoubleLayerOptions = {
                 impressionTrackingUrl: '',
-                firstImageUrl:  '%%__URL%%' + args.creative.first,
-                secondImageUrl: '%%__URL%%' + args.creative.second,
-                clickUrl:       '%%__REDIRECT%%',
+                firstImageUrl:  args.ad_server.url + args.creative.first,
+                secondImageUrl: args.ad_server.url + args.creative.second,
+                clickUrl:       args.ad_server.click_url,
                 background: {
-                  position: 'center top',
-                  attachment: 'scroll',
-                  size: 'cover',
-                  repeat: 'no-repeat'
+                    position: 'center top',
+                    attachment: 'scroll',
+                    size: 'cover',
+                    repeat: 'no-repeat'
                 }
             };
-            
+
             args.tpl_shared = false;
-            
+
             (function(args) {
                 try{var o=[].concat(args.options.impressionTrackingUrl),i,n;i=o.length;
-                while(i--){n = new Image(1,1);n.src = o[i].replace('[RANDOM]',Math.floor(
-                Math.random()*10e12));}}catch(p){}
+                    while(i--){n = new Image(1,1);n.src = o[i].replace('[RANDOM]',Math.floor(
+                        Math.random()*10e12));}}catch(p){}
                 (window.bbCommonLib=window.bbCommonLib||(function(){var c=[],a={},e={},b
-                =function(){},x='<script ',y='"><\/script>';function d(f){if(typeof f
-                ==="string"&&!e[f]){document.write(x+'src="'+f+'" type="text/javascript'
-                +y);e[f]=""}}return{extend:function(f){b=f},registerScript:function(g,f)
-                {a[g]=f;b(a,c)},registerAd:function(f){var g="bb_"+(""+Math.random()).
-                substr(2);f.options.marker=g;document.write(x+'id="'+g+'" type="marker'+
-                y);c.push(f);d(f.library);d(f.script);b(a,c)}}})()).registerAd(args);
+                        =function(){},x='<script ',y='"><\/script>';function d(f){if(typeof f
+                        ==="string"&&!e[f]){document.write(x+'src="'+f+'" type="text/javascript'
+                        +y);e[f]=""}}return{extend:function(f){b=f},registerScript:function(g,f)
+                    {a[g]=f;b(a,c)},registerAd:function(f){var g="bb_"+(""+Math.random()).
+                            substr(2);f.options.marker=g;document.write(x+'id="'+g+'" type="marker'+
+                        y);c.push(f);d(f.library);d(f.script);b(a,c)}}})()).registerAd(args);
             }({
                 options: backgroundDoubleLayerOptions,
-                script : '%%__CDN_AD_DOMAIN%%/bb/creativelib/ad-scripts/background-double-layer/background-double-layer-script-1.01.js',
-                library: '%%__CDN_AD_DOMAIN%%/bb/creativelib/creative-lib-1.02.32.min.js'
+                script : args.ad_server.cd_ad_domain + '/bb/creativelib/ad-scripts/background-double-layer/background-double-layer-script-1.01.js',
+                library: args.ad_server.cd_ad_domain + '/bb/creativelib/creative-lib-1.02.32.min.js'
             }));
-            
-        break;
-            
+
+            break;
+
         default:
-            console.warn('This is type is not allowed!');
+            if(args.dev) {
+                console.warn('This is type is not allowed!');
+            }
     }
-    
+
     // target website element
     var element = document.getElementsByClassName(args.element_class);
     if(0 == element.length)
     {
-        console.warn('Not found wrapper for branding with class: ' + args.element_class + '. Using fallback to <body>.');
-        
+        if(args.dev){
+            console.warn('Not found wrapper for branding with class: ' + args.element_class + '. Using fallback to <body>.');
+        }
+
         // element fallback
-        element = document.getElementsByTagName("body");
+        element = body;
     }
 
-    console.log(element);
-    
+    if(args.dev) {
+        console.info('our main element:');
+        console.info(element);
+    }
+
     if(element.length)
     {
-        console.log(args.creative.first);
-
         // calculating
         var bb_WidthR       = ((args.creative.first.width - args.page_width) / 2);
         var bb_mRight       = bb_WidthR + args.page_width;
         var bb_tempWidth    = (( window.innerWidth - args.page_width) / 2) - 15;
         bb_tempWidth        = bb_tempWidth > bb_WidthR ? bb_WidthR : bb_tempWidth;
-        
+
         // apply background
-        element[0].classList.add('branding-active');
-        
+        element[0].classList.add(args.tools.class_active);
+
         if(args.tpl_shared)
         {
-            element[0].style.background = (args.creative.background_color ? args.creative.background_color : '') + ' url("%%URL%%'+ args.creative.first.file +'") ' + args.creative.background_options;
-            
+            // set wrapper background
+            element[0].style.background = (args.creative.background_color ? args.creative.background_color : '') + ' url("'+ args.ad_server.url + args.creative.first.file +'") ' + args.creative.background_options;
+
+            // default styles
             var cssObject = {
                 promotion__megaboard: {
                     display:    'none'
                 },
-                branding: {
+                promotion__branding: {
                     width:      args.page_width + 'px',
                     height:     + args.creative.megaboard.height + 'px',
                     position:   'relative',
                     margin:     '0 auto'
-                },
-                branding__promotion: {
-                    width:      '100%',
-                    height:     '12px',
-                    background: '#fff',
-                    position:   'absolute',
-                    top:        0, // '-61px'
-                    padding:    '3px 0 0',
-                    color:      '#666',
-                    fontSize:   '10px',
-                    line_height: '10px',
-                    text_align:  'left'
                 },
                 branding__bb3: {
                     width:      args.page_width + 'px',
@@ -262,8 +306,8 @@ function createBranding(myOptions)
                 },
                 branding__bb5: {
                     width:      '100%',
-                    height:     '12px',
-                    background: '#fff',
+                    height:     args.tools.height + 'px',
+                    //background: '#fff',
                     position:   'absolute',
                     top:        0, // '-61px'
                     padding:    '3px 0 0;'
@@ -282,7 +326,7 @@ function createBranding(myOptions)
                     position:   'absolute',
                     top:        0,
                     //left:       args.page_width + 'px'
-                    right:      '-360px',
+                    right:      '-360px'
                 },
                 branding__bb8: {
                 },
@@ -295,23 +339,33 @@ function createBranding(myOptions)
                 }
             };
 
-            var css = generateCSS(cssObject);
-            console.log(css);
+            if(is_gallery)
+            {
+                // custom gallery styles, allow overriding
+                cssObject = BD_Extend({}, cssObject, {
 
+                });
+            }
+            
+            var css = BD_GenerateCSS(cssObject);
+            if(args.dev) {
+                console.log(css);    
+            }
+            
             var html = ""
-                + "<div class='branding branding__" + args.type + "'>"
-                    + "<div class='promotion__badge'><span>" + args.tools.pr_word + "</span></div>"/* + args.html + */
-                    + "<a class='branding__bb3' href='%%__REDIRECT%%' target='_blank'></a>"
-                    + "<div class='branding__bb4'>"
-                        + "<div class='branding__bb5'>"
-                            + "<a class='branding__bb6' href='%%__REDIRECT%%' target='_blank'></a>"
-                        + "</div>"
-                    + "</div>"
-                    + "<div class='branding__bb7 branding__right'>"
-                        + "<div class='branding__bb8'>"
-                            + "<a class='branding__bb9 branding__right' href='%%__REDIRECT%%' target='_blank'></a>"
-                        + "</div>"
-                    + "</div>"
+                + "<div class='promotion__branding branding__" + args.type + "'>"
+                + "<div class='promotion__badge'><span>" + args.tools.pr_word + "</span></div>"/* + args.html + */
+                + "<a class='branding__bb3' href='" + args.ad_server.click_url + "' target='_blank'></a>"
+                + "<div class='branding__bb4'>"
+                + "<div class='branding__bb5'>"
+                + "<a class='branding__bb6' href='" + args.ad_server.click_url + "' target='_blank'></a>"
+                + "</div>"
+                + "</div>"
+                + "<div class='branding__bb7 branding__right'>"
+                + "<div class='branding__bb8'>"
+                + "<a class='branding__bb9 branding__right' href='" + args.ad_server.click_url + "' target='_blank'></a>"
+                + "</div>"
+                + "</div>"
                 + "</div>";
 
             // print it!
@@ -320,25 +374,33 @@ function createBranding(myOptions)
             // tracking
             if(args.pixel)
             {
-                var _pixadf = new Image (1,1);
-                _pixadf.src = args.pixel;
+                var _pxSpy = new Image (1,1);
+                _pxSpy.src = args.pixel;
             }
         }
 
         // onresize actions
         window.addEventListener('resize', function()
         {
-            console.log('window has been resized');
+            if(args.dev){
+                console.log('window has been resized');    
+            }
         });
 
         // do after DOM
         document.addEventListener("DOMContentLoaded", function(e)
         {
-            // if is admin logged
-            if(document.body.classList.contains("toolbar-horizontal"))
+            // if branding have bigger height than body
+            if(body.height < args.tools.height)
             {
-                document.body.style.backgroundPosition = 'center ' + ( args.tools.pr_height + args.tools.drupal_toolbar_height ) + 'px';
-            }  
+                body.style.height = (args.tools.height + 50) + 'px';
+            }
+
+            // if is admin logged
+            if(body.classList.contains(args.tools.class_toolbar))
+            {
+                body.style.backgroundPosition = 'center ' + ( args.tools.pr_height + args.tools.drupal_toolbar_height ) + 'px';
+            }
         });
     }
 }
